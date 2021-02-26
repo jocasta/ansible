10b2b0a34693f6781bdbbf2eae84fdde38a71f89
Mike Gale <m.j.gale@gmail.com>
2021-02-26 15:21:29 +0000

    fixed script

--- inventory/vagrant_host_inventory.sh
+++ inventory/vagrant_host_inventory.sh
@@ -3,17 +3,11 @@
 ## REMOVE EXISTING FILE
 rm -f vagrant_hosts
 
-## VARIABLES
-echo "\
-[vars]
-ansible_connection=ssh
-ansible_user=ansible" >> vagrant_hosts
-
 ## CREATE POSTGRES GROUP
 echo -e "\n[postgres_servers]" >> vagrant_hosts
 
 ## ADD POSTGRES SERVERS
-cat /etc/hosts | grep postgres >> vagrant_hosts
+grep -o "postgres.*" /etc/hosts >> vagrant_hosts
 
 
 
